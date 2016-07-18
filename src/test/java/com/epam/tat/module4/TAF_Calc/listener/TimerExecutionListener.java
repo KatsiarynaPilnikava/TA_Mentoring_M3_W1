a5e8e06bbb728ff8ba20db3245f8c0f5b53c06ff
Katsiaryna Pilnikava <Katsiaryna_Pilnikava@epam.com>
2016-07-18 11:33:25 +0300

    listener updated

--- src/test/java/com/epam/tat/module4/TAF_Calc/listener/TimerExecutionListener.java
+++ src/test/java/com/epam/tat/module4/TAF_Calc/listener/TimerExecutionListener.java
@@ -1,29 +1,60 @@
 package com.epam.tat.module4.TAF_Calc.listener;
 
-import org.testng.IExecutionListener;
 import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
 import org.testng.Reporter;
- 
-public class TimerExecutionListener implements IExecutionListener {
-    private long startTime;
-    public void onExecutionStart() {
-        startTime = System.currentTimeMillis();
-        System.out.println("TestNG is going to start");    
-    }
 
-    public void onExecutionFinish() {
-        System.out.println("TestNG has finished, took around " + (System.currentTimeMillis() - startTime) + "ms");
-    }
-    public void onStart(ITestContext arg0) {
-    	 
-		Reporter.log("About to begin executing Test " + arg0.getName(), true);
- 
+public class TimerExecutionListener implements ITestListener {
+	private long startTime;
+
+	public void onExecutionStart() {
+		startTime = System.currentTimeMillis();
+		System.out.println("TestNG is going to start");
 	}
-    public void onFinish(ITestContext arg0) {
-    	 
+
+	public void onExecutionFinish() {
+		System.out.println("TestNG has finished, took around "
+				+ (System.currentTimeMillis() - startTime) + "ms");
+	}
+
+	public void onStart(ITestContext arg0) {
+
+		Reporter.log("About to begin executing Test " + arg0.getName(), true);
+
+	}
+
+	public void onFinish(ITestContext arg0) {
+
 		Reporter.log("Completed executing test " + arg0.getName(), true);
- 
+
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+
+		Reporter.log("Test failed within success persentage: " + arg0.getName());
+
+	}
+
+	public void onTestFailure(ITestResult arg0) {
+		Reporter.log("Test failed " + arg0.getName());
+
+	}
+
+	public void onTestSkipped(ITestResult arg0) {
+		Reporter.log("Test skipped " + arg0.getName());
+
+	}
+
+
+	public void onTestSuccess(ITestResult arg0) {
+		System.out.println("-> onTestSuccess: " + arg0.getName() + " " + (arg0.getEndMillis() - arg0.getStartMillis() + " ms"));
+
+	}
+
+	public void onTestStart(ITestResult arg0) {
+		Reporter.log("Test started " + arg0.getName());
+
 	}
 
 }
-
